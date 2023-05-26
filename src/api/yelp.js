df4f707ce9e713a1d116f2e508fe58e04d8884b4
rihab <rihbalyasiri@yahoo.com>
2023-05-26 09:11:47 +0200

    create axios for https requests

---
+++ src/api/yelp.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default axios.create({
+  basedURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      "Bearer " +
+      "SLrm-A6kbKRDayotRq5f1kLNSBX3v-6u3pEkS1khXvbHCvUjHhIxoXNrgkwNt95pa1Qx-I8HMK84ZzzgT9T2oISLxNiufgqDcMd5fbpX_dm6UnSrkWiJsJg9tllwZHYx",
+  },
+});
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelpAPI from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
   return (
     <View>
-          <SearchBar term={term}
-              onChangeTerm={(newTerm) => setTerm(newTerm)}
-              onTermSubmit={() => console.log(term) } />
+      <SearchBar
+        term={term}
+        onChangeTerm={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log(term)}
+      />
       <Text>{term}</Text>
     </View>
   );
